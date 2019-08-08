aa2c819013f91bfd8a4e3eea8d3abcfd572f135a
ivan <iizdepskyy112@gmail.com>
2019-08-08 10:51:45 +0300

    244. Routing and Server Deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/my-app">
       <div className="App">
         <Blog />
       </div>
@@ -16,4 +16,3 @@ class App extends Component {
 }
 
 export default App;
-
